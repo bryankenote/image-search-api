229050a52aa1d3be3c5337a8bf1eda317648aa31
Bryan <brykenote@gmail.com>
2017-03-19 01:43:15 +0000

    add and retrieve record in database for a search

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,35 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Image Search api' });
 });
 
+router.get('/api/latest/imagesearch', function (req, res) {
+    var db = req.db;
+    var collection = db.get('recentCollection');
+    collection.find({},{fields:{_id:0}}, function (err, docs) {
+        if (err)
+            res.send('There was a problem completing your request');
+        else
+            res.send(docs);
+    });
+});
+
+router.get('/api/imagesearch/:term', function (req, res) {
+    var db = req.db;
+    var offset = req.query.offset;
+    
+    var entry = {
+      "term": req.params.term,
+      "when": new Date().toISOString()
+    };
+    
+    //res.send(JSON.stringify(entry));
+    
+    var collection = db.get('recentCollection');
+    collection.insert(entry, function (err, docs) {
+        if (err)
+            res.send("There was a problem completing your search.");
+        else
+            res.send(docs);
+    });
+});
+
 module.exports = router;
